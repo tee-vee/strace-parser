711f4dfc003ff8f5cd65c6934ae43c2e642804e7
Will Chandler <wchandler@gitlab.com>
2019-04-18 16:44:15 -0400

    Add missed files

---
+++ src/cli.rs
@@ -0,0 +1,246 @@
+use crate::Pid;
+use clap::{App, AppSettings, Arg, ArgMatches, SubCommand};
+
+pub fn cli_args<'a>() -> ArgMatches<'a> {
+    App::new("strace parser")
+        .version(clap::crate_version!())
+        .author(clap::crate_authors!())
+        .about("Summarizes raw strace output")
+        .setting(AppSettings::SubcommandRequired)
+        .setting(AppSettings::InferSubcommands)
+        .setting(AppSettings::VersionlessSubcommands)
+        .setting(AppSettings::ColoredHelp)
+        .arg(
+            Arg::with_name("INPUT")
+                .help("File to be parsed")
+                .required(true)
+                .takes_value(true)
+                .number_of_values(1),
+        )
+        .subcommand(SubCommand::with_name("details")
+            .about("Details of PID(s) including syscalls stats, exec'd process, and slowest 'open' calls")
+            .arg(
+                Arg::with_name("pid")
+                    .short("p")
+                    .long("pid")
+                    .help("PIDs to analyze")
+                    .required(true)
+                    .takes_value(true)
+                    .value_name("PIDS")
+                    .multiple(true)
+                    .validator(validate_pid),
+            )
+            .arg(
+                Arg::with_name("related")
+                    .short("r")
+                    .long("related")
+                    .help("Include parent and child PIDs of <PIDS> in results"),
+            ))
+        .subcommand(SubCommand::with_name("exec")
+            .about("List programs executed")
+            .arg(
+                Arg::with_name("pid")
+                    .short("p")
+                    .long("pid")
+                    .help("PIDS to analyze")
+                    .takes_value(true)
+                    .value_name("PIDS")
+                    .multiple(true)
+            )
+            .arg( Arg::with_name("related")
+                    .short("r")
+                    .long("related")
+                    .help("Include parent and child PIDs of <PIDS> in results"),
+            ))
+        .subcommand(SubCommand::with_name("files")
+            .about("List files opened")
+            .arg(
+                Arg::with_name("pid")
+                    .short("p")
+                    .long("pid")
+                    .help("PIDs to analyze")
+                    .takes_value(true)
+                    .value_name("PIDS")
+                    .multiple(true)
+                    .validator(validate_pid),
+            )
+            .arg( Arg::with_name("related")
+                    .short("r")
+                    .long("related")
+                    .help("Include parent and child PIDs of <PIDS> in results"),
+            )
+            .arg(
+                Arg::with_name("sort_by")
+                    .short("s")
+                    .long("sort")
+                    .help("Field to sort results by")
+                    .takes_value(true)
+                    .value_name("SORT_BY")
+                    .possible_values(&[
+                        "duration",
+                        "pid",
+                        "time",
+                    ]),
+            ))
+        .subcommand(SubCommand::with_name("histogram")
+            .about("List files opened")
+            .arg(
+                Arg::with_name("syscall")
+                    .short("S")
+                    .long("syscall")
+                    .help("Syscall to analyze")
+                    .required(true)
+                    .value_name("SYSCALL")
+                    .takes_value(true)
+                    .number_of_values(1),
+            )
+            .arg(
+                Arg::with_name("pid")
+                    .short("p")
+                    .long("pid")
+                    .help("PIDs to analyze")
+                    .takes_value(true)
+                    .value_name("PIDS")
+                    .multiple(true)
+                    .validator(validate_pid),
+            )
+            .arg(
+                Arg::with_name("related")
+                    .short("r")
+                    .long("related")
+                    .help("Include parent and child PIDs of <PIDS> in results"),
+            ))
+        .subcommand(SubCommand::with_name("io")
+            .about("List details of I/O syscalls, including read, write, sendmsg, recvmsg, and others")
+            .arg(
+                Arg::with_name("pid")
+                    .short("p")
+                    .long("pid")
+                    .help("Comma separated list PIDs to analyze")
+                    .takes_value(true)
+                    .value_name("PIDS")
+                    .multiple(true)
+                    .validator(validate_pid),
+            )
+            .arg(
+                Arg::with_name("sort_by")
+                    .short("s")
+                    .long("sort")
+                    .help("Field to sort results by")
+                    .takes_value(true)
+                    .value_name("SORT_BY")
+                    .possible_values(&[
+                        "duration",
+                        "pid",
+                        "time",
+                    ]),
+            )
+            .arg( Arg::with_name("related")
+                    .short("r")
+                    .long("related")
+                    .help("Include parent and child PIDs of <PIDS> in results"),
+            ))
+        .subcommand(SubCommand::with_name("list")
+            .about("List of PIDs with details of syscall performance")
+            .arg(
+                Arg::with_name("count")
+                    .short("c")
+                    .long("count")
+                    .help("The number of PIDs to print")
+                    .takes_value(true)
+                    .value_name("COUNT")
+                    .default_value_if("detail", None, "5")
+                    .validator(validate_count),
+            )
+            .arg(
+                Arg::with_name("pid")
+                    .short("p")
+                    .long("pid")
+                    .help("Comma separated list PIDs to analyze")
+                    .takes_value(true)
+                    .value_name("PIDS")
+                    .multiple(true)
+                    .validator(validate_pid),
+            )
+            .arg(
+                Arg::with_name("related")
+                    .short("r")
+                    .long("related")
+                    .help("Include parent and child PIDs of <PIDS> in results"),
+            )
+            .arg(
+                Arg::with_name("sort_by")
+                    .short("s")
+                    .long("sort")
+                    .help("Field to sort results by")
+                    .takes_value(true)
+                    .value_name("SORT_BY")
+                    .possible_values(&[
+                        "active_time",
+                        "children",
+                        "pid",
+                        "syscalls",
+                        "total_time",
+                        "user_time",
+                    ]),
+            ))
+        .subcommand(SubCommand::with_name("summary")
+            .about("Overview of PIDs in session")
+            .arg(
+                Arg::with_name("count")
+                    .short("c")
+                    .long("count")
+                    .help("The number of PIDs to print")
+                    .takes_value(true)
+                    .value_name("COUNT")
+                    .default_value_if("detail", None, "5")
+                    .validator(validate_count),
+            )
+            .arg(
+                Arg::with_name("pid")
+                    .short("p")
+                    .long("pid")
+                    .help("Comma separated list PIDs to analyze")
+                    .takes_value(true)
+                    .value_name("PIDS")
+                    .multiple(true)
+                    .validator(validate_pid),
+            )
+            .arg(
+                Arg::with_name("related")
+                    .short("r")
+                    .long("related")
+                    .help("Include parent and child PIDs of <PIDS> in results"),
+            )
+            .arg(
+                Arg::with_name("sort_by")
+                    .short("s")
+                    .long("sort")
+                    .help("Field to sort results by")
+                    .takes_value(true)
+                    .value_name("SORT_BY")
+                    .possible_values(&[
+                        "active_time",
+                        "children",
+                        "pid",
+                        "syscalls",
+                        "total_time",
+                        "user_time",
+                    ]),
+            ))
+        .get_matches()
+}
+
+fn validate_pid(p: String) -> Result<(), String> {
+    if p.parse::<Pid>().is_ok() {
+        return Ok(());
+    }
+    Err(String::from("PID must be an integer"))
+}
+
+fn validate_count(c: String) -> Result<(), String> {
+    if c.parse::<usize>().is_ok() {
+        return Ok(());
+    }
+    Err(String::from("COUNT must be a non-negative integer"))
+}
---
+++ strace-parse/src/time.rs
@@ -0,0 +1,57 @@
+use chrono::{Duration, NaiveDateTime, NaiveTime};
+
+pub fn parse_elapsed_real_time(buffer: &str) -> Option<Duration> {
+    let start_token = buffer
+        .lines()
+        .next()
+        .map(|line| line.split_whitespace())
+        .and_then(|split| split.skip(1).next());
+
+    let end_token = buffer
+        .lines()
+        .next_back()
+        .map(|line| line.split_whitespace())
+        .and_then(|split| split.skip(1).next());
+
+    match (start_token, end_token) {
+        (Some(start), Some(end)) => {
+            let start_time;
+            if let Ok(t) = NaiveTime::parse_from_str(start, "%H:%M:%S%.6f") {
+                start_time = Some(t);
+            } else if let Some(t) = parse_unix_timestamp(start) {
+                start_time = Some(t.time());
+            } else {
+                start_time = None;
+            }
+
+            let end_time;
+            if let Ok(t) = NaiveTime::parse_from_str(end, "%H:%M:%S%.6f") {
+                end_time = Some(t);
+            } else if let Some(t) = parse_unix_timestamp(end) {
+                end_time = Some(t.time());
+            } else {
+                end_time = None;
+            }
+            match (start_time, end_time) {
+                (Some(starting), Some(ending)) => Some(ending - starting),
+                _ => None,
+            }
+        }
+        _ => None,
+    }
+}
+
+pub fn parse_unix_timestamp(time: &str) -> Option<NaiveDateTime> {
+    let mut split_iter = time.split('.');
+
+    let secs = match split_iter.next().and_then(|s| s.parse::<i64>().ok()) {
+        Some(s) => s,
+        None => return None,
+    };
+    let nsecs = match split_iter.next().and_then(|n| n.parse::<u32>().ok()) {
+        Some(n) => n * 1000, // strace provides usecs, convert to nsecs
+        None => return None,
+    };
+
+    NaiveDateTime::from_timestamp_opt(secs, nsecs)
+}
